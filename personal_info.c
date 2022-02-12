aae9247e5c93b4fccba0916645166814550c33a3
Asharani-star <99533998+Asharani-star@users.noreply.github.com>
2022-02-12 15:29:47 +0530

    Add files via upload

---
+++ personal_info.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("name-Asharani patra");
+    printf("\nregd no.-081");
+    printf("\nbranch-Electronics and telecommunication");
+    printf("\nhobbies-reading novel,listenting music,drawing");
+    return 0;
+}
